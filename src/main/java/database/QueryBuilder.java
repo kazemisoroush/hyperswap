51c6b90a54ddd228b2f20ceb971302211cd2404a
Soroush Kazemi <kazemi.soroush@gmaill.com>
2016-12-01 13:48:12 +0330

    adds doc block to query builder class.

--- src/main/java/database/QueryBuilder.java
+++ src/main/java/database/QueryBuilder.java
@@ -7,22 +7,49 @@ import java.util.ArrayList;
 
 public class QueryBuilder {
 
+    /**
+     * Resulting query string.
+     */
     protected String query;
 
+    /**
+     * Type of the query.
+     */
     protected QueryType queryType;
 
+    /**
+     * Tables in which query is being executed.
+     */
     protected ArrayList<String> tables;
 
+    /**
+     * Array of where conditions. Used in select, update and delete queries.
+     */
     protected ArrayList<String> whereConditions;
 
+    /**
+     * Array of values being inserted into database.
+     */
     protected ArrayList<String> values;
 
+    /**
+     * Array of attribute / value pairs in string form for update queries.
+     */
     protected ArrayList<String> set;
 
+    /**
+     * Columns which are being selected.
+     */
     protected ArrayList<String> columns;
 
+    /**
+     * Database connector used to execute queries.
+     */
     protected DatabaseConnector connector;
 
+    /**
+     * Instantiate new instance of query builder.
+     */
     public QueryBuilder() {
         // clean up the query builder...
         this.cleanUp();
@@ -34,6 +61,9 @@ public class QueryBuilder {
         this.connector.connect();
     }
 
+    /**
+     * Clean up attributes of this query builder and make it ready for another query.
+     */
     private void cleanUp() {
         this.query = "";
         this.queryType = QueryType.SELECT;
@@ -44,6 +74,13 @@ public class QueryBuilder {
         this.columns = new ArrayList<String>();
     }
 
+    /**
+     * Start a select query.
+     *
+     * @param columns which are being selected.
+     *
+     * @return this to continue chaining.
+     */
     public QueryBuilder select(String... columns) {
         this.queryType = QueryType.SELECT;
 
@@ -54,24 +91,46 @@ public class QueryBuilder {
         return this;
     }
 
+    /**
+     * Start an insert query.
+     *
+     * @return this to continue chaining.
+     */
     public QueryBuilder insert() {
         this.queryType = QueryType.INSERT;
 
         return this;
     }
 
+    /**
+     * Start a delete query.
+     *
+     * @return this to continue chaining.
+     */
     public QueryBuilder delete() {
         this.queryType = QueryType.DELETE;
 
         return this;
     }
 
+    /**
+     * Start an update query.
+     *
+     * @return this to continue chaining.
+     */
     public QueryBuilder update() {
         this.queryType = QueryType.UPDATE;
 
         return this;
     }
 
+    /**
+     * Define tables which we are using to select, update or delete.
+     *
+     * @param tables names which are being affected.
+     *
+     * @return this to continue chaining.
+     */
     public QueryBuilder from(String... tables) {
         for (String table : tables) {
             this.tables.add(table);
@@ -80,12 +139,27 @@ public class QueryBuilder {
         return this;
     }
 
+    /**
+     * Define table which is used to execute insert.
+     *
+     * @param table name which is being affected.
+     *
+     * @return this to continue chaining.
+     */
     public QueryBuilder into(String table) {
         this.tables.add(table);
 
         return this;
     }
 
+    /**
+     * Set values for update queries.
+     *
+     * @param column name to change value.
+     * @param value  to be changed.
+     *
+     * @return this to continue chaining.
+     */
     public QueryBuilder set(String column, String value) {
         // make the set statement and add it to set array...
         this.set.add(column + " = " + value);
@@ -93,6 +167,13 @@ public class QueryBuilder {
         return this;
     }
 
+    /**
+     * Set values to be inserted inside the database.
+     *
+     * @param columns values to be inserted.
+     *
+     * @return this to continue chaining.
+     */
     public QueryBuilder values(String... columns) {
         // add the value to the list of values...
         for (String column : columns) {
@@ -102,6 +183,15 @@ public class QueryBuilder {
         return this;
     }
 
+    /**
+     * Add where condition to the query.
+     *
+     * @param column   of condition.
+     * @param equality term of condition.
+     * @param value    to be inserted.
+     *
+     * @return this to continue chaining.
+     */
     public QueryBuilder where(String column, String equality, String value) {
         // add the where clause...
         this.whereConditions.add(column + " " + equality + " " + value);
@@ -109,6 +199,12 @@ public class QueryBuilder {
         return this;
     }
 
+    /**
+     * Execute select queries.
+     *
+     * @return two dimensional array of result.
+     * @throws IncorrectQueryExecution for incorrect uses.
+     */
     public ArrayList<ArrayList<String>> get() throws IncorrectQueryExecution {
         // check the query type...
         if (this.queryType != QueryType.SELECT) {
@@ -131,6 +227,12 @@ public class QueryBuilder {
         return result;
     }
 
+    /**
+     * Execute and commit the insert, update and delete queries.
+     *
+     * @return result of execution in integer format.
+     * @throws IncorrectQueryExecution for incorrect uses.
+     */
     public int save() throws IncorrectQueryExecution {
         // check the query type...
         if (this.queryType != QueryType.INSERT && this.queryType != QueryType.UPDATE && this.queryType != QueryType.DELETE) {
